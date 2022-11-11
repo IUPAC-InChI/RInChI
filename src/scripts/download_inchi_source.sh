8d36abaeab061543e7bee3ee2b3c8fe6877b3733
Jan Holst Jensen <jan@biochemfusion.com>
2022-11-11 11:01:02 +0100

    Remove duplicated scripts/ folder.

--- src/scripts/download_inchi_source.sh
+++
@@ -1,7 +0,0 @@
-wget https://www.inchi-trust.org/download/104/INCHI-1-API.zip
-
-## If download fails with an expired certificate, use this command instead.
-## wget https://www.inchi-trust.org/download/104/INCHI-1-API.zip --no-check-certificate
-
-unzip INCHI-1-API.zip
-
